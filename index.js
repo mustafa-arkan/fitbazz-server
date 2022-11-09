64eb48dacee2d66bea188d56dde9ceb059bb0f7a
mustafa-arkan <arkancse@gmail.com>
2022-11-09 23:43:59 +0600

    details service add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const port=process.env.PORT||5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
@@ -37,6 +37,21 @@ app.get('/services',async(req,res)=>{
   const services=await cursor.toArray()
   res.send(services)
   })
+
+  app.get('/services/:id', async(req,res)=>{
+
+    const id=req.params.id 
+    const query={_id: ObjectId(id)}
+    
+    const service=await serviceCollection.findOne(query)
+    res.send(service)
+    
+    })
+
+
+
+
+
 
 
 //add service api
